d3a37d520b4356cf74beed53d3cc4a4667696cef
elvinos <aep.charles@gmail.com>
2017-04-06 15:01:32 +0100

    added part 2a script and sim

---
+++ Phase2/Code/part2a.m
@@ -0,0 +1,73 @@
+close all force;
+clear all;
+clc;
+
+%% 2nd Order Transfer Function
+%tf([1.109],[1 0.1313 1.109])
+%-2 to 2, 0.26
+
+%including our observed k from previous task and degree converter
+num=[0.26*1.109*180/pi()];
+den=[1 0.1313 1.109];
+A = tf(num,den);
+t = 0:0:100;
+
+set(0,'DefaultFigureWindowStyle','docked') %% Dock all figures
+set(0,'defaultfigurecolor',[1 1 1]) % Set bacground colour to white
+set(0,'DefaultAxesFontSize', 16)
+set(0,'DefaultTextFontSize', 18)
+
+%% Gain iterating
+
+% stepP=0.1;
+stepP=0.1;
+Gg = (0.0001:stepP:0.0001);
+maxGg=max(Gg);
+minGg=min(Gg);
+
+Legend=cell(length(Gg),1);
+   
+for p=1:length(Gg)
+    Legend{p}=num2str(Gg(p));
+end
+
+j = 1;
+for G = minGg:stepP:maxGg
+
+   Kd= G;
+   Kp= 0;
+   Ki= 0;
+sim('PIDp2.slx',[0 20])
+   
+         Cpid = pid(Kd,Ki,Ki);
+         Tpid = feedback(Cpid*A,1);
+         stepinfoKPID(j) = stepinfo(Tpid);
+
+        figure(72)
+        plot(varyKIKDKP);
+        title('Response For PID');
+        hold on;
+        figure(73)
+        pzmap(Tpid);
+        title('Root Locus for PID')
+        grid on;
+        hold on;
+         
+        j = j + 1;
+        
+end
+
+Effect_of_Varying_KPID = struct2table(stepinfoKPID);
+
+for i=72:73
+    figure(i)
+    
+    if i < 73
+        xlabel('Time/ Seconds')
+        ylabel('Response')
+       legend(Legend,'Orientation','vertical','Location','southeast')
+
+    else
+        legend(Legend,'Orientation','vertical','Location','northwest')
+    end
+end 
